64e2a80d25789fa1039876b3399c979a1ddf0fbd
Dev-FStacker <bennguyen.contact@gmail.com>
2025-02-08 13:16:23 +0700

    Initial: mobile app and webapp

---
+++ FE/MobileApp/SpeakAI/SpeakAI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SpeakAI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
