00b72bb1f9deb89462d35985c7ddd183fc73e5ea
doforTri <trihoangnguyenn@gmail.com>
2025-02-09 14:23:59 +0700

    CRUD: Course, Topic , Exercise

---
+++ BE/BLL/Interface/ICourseService.cs
@@ -0,0 +1,43 @@
+﻿using Common.DTO;
+using DTO.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interface
+{
+    public interface ICourseService
+    {
+        /// <summary>
+    /// Topic
+    /// </summary>
+    /// <param name="courseDto"></param>
+    /// <returns></returns>
+        Task<ResponseDTO> CreateCourseWithTopicsAndExercisesAsync(CreateCourseDTO courseDto);
+        Task<ResponseDTO> GetCourseByIdAsync(Guid courseId);
+        Task<ResponseDTO> UpdateCourseAsync(Guid courseId, UpdateCourseDTO courseDto);
+        Task<ResponseDTO> DeleteCourseAsync(Guid courseId);
+
+/// <summary>
+/// Topic 
+/// </summary>
+/// <param name="topicId"></param>
+/// <returns></returns>
+        Task<ResponseDTO> GetTopicByIdAsync(Guid topicId);
+        Task<ResponseDTO> AddTopicToCourseAsync(Guid courseId, CreateTopicDTO topicDto);
+        Task<ResponseDTO> UpdateTopicAsync(Guid topicId, UpdateTopicDTO topicDto);
+        Task<ResponseDTO> DeleteTopicAsync(Guid topicId);
+
+        /// <summary>
+        /// Exercise
+        /// </summary>
+        /// <param name="exerciseId"></param>
+        /// <returns></returns>
+        Task<ResponseDTO> GetExerciseByIdAsync(Guid exerciseId);
+        Task<ResponseDTO> AddExerciseToTopicAsync(Guid topicId, CreateExerciseDTO exerciseDto);
+        Task<ResponseDTO> UpdateExerciseAsync(Guid exerciseId, UpdateExerciseDTO exerciseDto);
+        Task<ResponseDTO> DeleteExerciseAsync(Guid exerciseId);
+    }
+}
---
+++ BE/Common/DTO/UpdateCourseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DTO
+{
+    public class UpdateCourseDTO
+    {
+        public string CourseName { get; set; }
+        public string Description { get; set; }
+        public decimal MaxPoint { get; set; }
+        public bool IsFree { get; set; }
+        public int LevelId { get; set; }
+    }
+}
