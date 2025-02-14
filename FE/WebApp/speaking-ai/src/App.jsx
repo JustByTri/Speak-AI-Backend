c9c615b44179eb03d8966f5d96cb923d986450b2
NET15_Quốc Thịnh <thinhpdqse171589@fpt.edu.vn>
2025-02-14 07:27:59 +0700

    Refactor: Update AdminPage

--- FE/WebApp/speaking-ai/src/App.jsx
+++ FE/WebApp/speaking-ai/src/App.jsx
@@ -1,14 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BookingDashboard from "./pages/BookingDashboard";
 import LoginPage from "./pages/LoginPage";
+import AdminPage from "./pages/AdminPage";
+import DashboardHome from "./pages/DashboardHome";
+import CoursePage from "./pages/course/CoursePage";
+// import BookingsPage from "./pages/BookingsPage";
+// import PaymentsPage from "./pages/PaymentsPage";
+// import ReportsPage from "./pages/ReportsPage";
+// import SettingsPage from "./pages/SettingsPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<BookingDashboard />} />
+        <Route element={<AdminPage />}>
+          <Route index element={<DashboardHome />} />
+          <Route path="/courses" element={<CoursePage />} />
+          {/* <Route path="/bookings" element={<BookingsPage />} />
+          <Route path="/payments" element={<PaymentsPage />} />
+          <Route path="/reports" element={<ReportsPage />} />
+          <Route path="/settings" element={<SettingsPage />} /> */}
+        </Route>
       </Routes>
     </Router>
   );
